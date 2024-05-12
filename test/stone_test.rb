c6003f6d5f8d966af42682cad23e732a31466ab2
Zai <130903099+zaibutcooler@users.noreply.github.com>
2024-05-13 06:23:01 +0630

    issue template etc

--- test/stone_test.rb
+++ test/stone_test.rb
@@ -1,10 +1,10 @@
 require 'minitest/autorun'
-require_relative '../lib/stone'
+require_relative '../lib/deepmine'
 
 # Test program coolness
 class CoolProgramTest < Minitest::Test
-  def test_coolness_off_the_charts
-    # skip
-    assert_equal CoolProgram.new.coolness, 11
-  end
+  # def test_coolness_off_the_charts
+  #   # skip
+  #   assert_equal CoolProgram.new.coolness, 11
+  # end
 end
