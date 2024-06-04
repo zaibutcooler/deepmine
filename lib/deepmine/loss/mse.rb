e3714cd7eaf38985584d0f385f4546778ef6b7d2
Zai <130903099+zaibutcooler@users.noreply.github.com>
2024-06-05 01:06:49 +0630

    added mse loss

--- lib/deepmine/loss/mse.rb
+++ lib/deepmine/loss/mse.rb
@@ -1,7 +1,13 @@
 module Deepmine
     module Loss
       class MSE
-        
+        def self.compute(pred, target)
+          Tensor.new([pred.data.zip(target.data).map { |p, t| (p - t)**2 }.sum / pred.data.size])
+        end
+  
+        def self.backward(pred, target)
+          pred.data.zip(target.data).map { |p, t| 2 * (p - t) / pred.data.size.to_f }
+        end
       end
     end
   end
